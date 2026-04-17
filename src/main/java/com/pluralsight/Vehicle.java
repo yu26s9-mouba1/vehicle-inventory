36557b32f1d3f77cd961b5306ec21b59e4a924a9
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-16 23:30:14 -0400

    Created getters and setters

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -8,7 +8,7 @@ public class Vehicle {
     private int odometerReading;
     private float price;
 
-    // Assigning all the properties of the vehicle
+    // Assigning all the properties of the vehicle with set and getters
     public Vehicle(long vehicleId, String makeModel, String color, int odometerReading, float price) {
         this.vehicleId = vehicleId;
         this.makeModel = makeModel;
@@ -17,12 +17,50 @@ public class Vehicle {
         this.price = price;
     }
 
-//    public long getVehicleId() {
-//        return vehicleId;
-//    }
+    public long getVehicleId() {
+        return vehicleId;
+    }
 
+    public void setVehicleId(long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
 
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
+
+
+
+
 
 
 
