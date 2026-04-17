a9db8c1a289c22207f8600e3ab29c810c0bbdbef
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-16 22:54:14 -0400

    Created a class that stores all the vehicle properties and another methoer to assign properties

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,7 +1,28 @@
 package com.pluralsight;
 
 public class Vehicle {
-    public static void main(String[] args) {
-        
+    //Creating a class that contains the vehicle identifications, making sure they are private
+    private long vehicleId;
+    private String makeModel;
+    private String color;
+    private int odometerReading;
+    private float price;
+
+    // Assigning all the properties of the vehicle
+    public Vehicle(long vehicleId, String makeModel, String color, int odometerReading, float price) {
+        this.vehicleId = vehicleId;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
     }
+
+//    public long getVehicleId() {
+//        return vehicleId;
+//    }
+
+
 }
+
+
+
